3b1fd69308c8b81298b3da122be242c56f8df5c3
jiri.kudrna <jiri.kudrna@uniprog.cz>
2021-12-08 13:54:47 +0100

    Day7

---
+++ Day7a/Program.cs
@@ -0,0 +1,11 @@
+﻿var input = File.ReadAllText("input.txt");
+var positions = input.Split(',').Select(x => int.Parse(x)).ToList();
+
+var median = positions.OrderBy(x=>x).ElementAt(positions.Count/2);
+
+var totalFuel = positions.Sum(x => Math.Abs(x - median));
+
+Console.WriteLine($"Result: {totalFuel}");
+
+
+
---
+++ Day7b/Program.cs
@@ -0,0 +1,18 @@
+﻿//BRUTAL FORCE :)
+
+var input = File.ReadAllText("input.txt");
+var positions = input.Split(',').Select(x => int.Parse(x)).OrderBy(x => x).ToList();
+Dictionary<int,int> targetAndCost = new Dictionary<int,int>();
+
+for (int i = positions.First(); i < positions.Last(); i++)
+{
+    targetAndCost.Add(i,GetFuelCost(positions, i));
+}
+
+Console.WriteLine($"Result: {targetAndCost.Values.Min()}");
+
+//functions
+int GetFuelCost(List<int> input, int targetPosition)
+{
+    return (int)positions.Sum(x => (1 + Math.Abs(x - targetPosition)) * (Math.Abs(x - targetPosition) / 2.0));
+}
