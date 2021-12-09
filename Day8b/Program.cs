752470182cd6734da7a1e56b3b868a44f3e65e30
jiri.kudrna <jiri.kudrna@uniprog.cz>
2021-12-09 07:57:06 +0100

    Day8 and 9 init

---
+++ Day8b/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var inputSignals = new List<string>();
+var outputSignals = new List<string>();
+
+foreach (var line in input)
+{
+    var inputOutput = line.Split('|');
+    inputSignals.Add(inputOutput[0]);
+    outputSignals.Add(inputOutput[1]);
+}
+var result = outputSignals.SelectMany(x => x.Trim().Split(' ')).Where(x => x.Length == 2 || x.Length == 4 || x.Length == 3 || x.Length == 7).Count();
+
+Console.WriteLine($"Result: {result}");
---
+++ Day9a/Program.cs
@@ -0,0 +1,39 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var map = new int[input.First().Length, input.Length];
+//load data into array
+for (int row = 0; row < input.Length; row++)
+{
+    for (int column = 0; column < input.First().Length; column++)
+    {
+        map[column, row] = Convert.ToInt32(input[row][column]) - Convert.ToInt32('0');
+    }
+
+}
+
+int totalSum = 0;
+for (int row = 0; row < input.Length; row++)
+{
+    for (int column = 0; column < input.First().Length; column++)
+    {
+        if (column + 1 < input.First().Length && map[column, row] >= map[column + 1, row]) continue;
+        if (column - 1 >= 0 && map[column, row] >= map[column - 1, row]) continue;
+        if (row + 1 < input.Length && map[column, row] >= map[column, row + 1]) continue;
+        if (row - 1 >= 0 && map[column, row] >= map[column, row - 1]) continue;
+
+        totalSum += map[column, row] + 1;
+    }
+    Console.WriteLine();
+}
+
+//debug print
+//for (int row = 0; row < input.Length; row++)
+//{
+//    for (int column = 0; column < input.First().Length; column++)
+//    {
+//        Console.Write($"{map[column,row]}");
+//    }
+//    Console.WriteLine();
+//}
+
+Console.WriteLine($"Result: {totalSum}");
