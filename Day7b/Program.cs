af8dd67be3788eca39a4b00fa82d4a64c6c67563
jiri.kudrna <jiri.kudrna@uniprog.cz>
2021-12-09 09:30:48 +0100

    9b and no brutal 7b

--- Day7b/Program.cs
+++ Day7b/Program.cs
@@ -4,12 +4,19 @@ var input = File.ReadAllText("input.txt");
 var positions = input.Split(',').Select(x => int.Parse(x)).OrderBy(x => x).ToList();
 Dictionary<int,int> targetAndCost = new Dictionary<int,int>();
 
+
+
 for (int i = positions.First(); i < positions.Last(); i++)
 {
     targetAndCost.Add(i,GetFuelCost(positions, i));
 }
 
+//no prutal force version
+//var avg = positions.Average(x => x);
+//var cost = GetFuelCost(positions, (int)avg);
+
 Console.WriteLine($"Result: {targetAndCost.Values.Min()}");
+
 
 //functions
 int GetFuelCost(List<int> input, int targetPosition)
---
+++ Day9b/Program.cs
@@ -0,0 +1,61 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var map = new int[input.First().Length, input.Length];
+//load data into array
+for (int row = 0; row < input.Length; row++)
+{
+    for (int column = 0; column < input.First().Length; column++)
+    {
+        map[column, row] = Convert.ToInt32(input[row][column]) - Convert.ToInt32('0');
+    }
+
+}
+
+int totalSum = 0;
+for (int row = 0; row < input.Length; row++)
+{
+    for (int column = 0; column < input.First().Length; column++)
+    {
+        if (column + 1 < input.First().Length && map[column, row] >= map[column + 1, row]) continue;
+        if (column - 1 >= 0 && map[column, row] >= map[column - 1, row]) continue;
+        if (row + 1 < input.Length && map[column, row] >= map[column, row + 1]) continue;
+        if (row - 1 >= 0 && map[column, row] >= map[column, row - 1]) continue;
+
+        totalSum += map[column, row] + 1;
+    }
+    Console.WriteLine();
+}
+
+//debug print
+//for (int row = 0; row < input.Length; row++)
+//{
+//    for (int column = 0; column < input.First().Length; column++)
+//    {
+//        Console.Write($"{map[column,row]}");
+//    }
+//    Console.WriteLine();
+//}
+
+Console.WriteLine($"Result: {totalSum}");
+
+
+
+void FloodFill(int[,] map, int column,int row)
+{
+    Stack<(int,int)> pixels = new Stack<(int, int)>();
+    var value = map[column,row];
+    pixels.Push((column, row));
+
+    while (pixels.Count > 0)
+    {
+        (int column, int row) a = pixels.Pop();
+        if(a.column >= 0 && a.column < map.GetLength(0) && a.row >= 0 && a.row < map.GetLength(1))
+        {
+            if(map[a.column,a.row] < map[a.column - 1, a.row]) pixels.Push(new (a.column - 1, a.row));
+            if (map[a.column, a.row] < map[a.column + 1, a.row]) pixels.Push(new (a.column + 1, a.row));
+            if (map[a.column, a.row] < map[a.column , a.row - 1]) pixels.Push(new (a.column, a.row - 1));
+            if (map[a.column, a.row] < map[a.column , a.row + 1]) pixels.Push(new (a.column, a.row + 1));
+        }
+    }
+    return;
+}
