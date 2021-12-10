47bdde288cfb2a529b4578370e8cabd807c05fc0
jiri.kudrna <jiri.kudrna@uniprog.cz>
2021-12-10 15:38:34 +0100

    Day10

---
+++ Day10a/Program.cs
@@ -0,0 +1,47 @@
+﻿var input = File.ReadAllLines("input.txt");
+var errroScore = 0;
+foreach (var line in input)
+{
+    var brackets = new Stack<char>();
+    for (int i = 0; i < line.Length; i++)
+    {
+        if(line[i] == '(' || line[i] == '[' || line[i] == '{' || line[i] == '<') brackets.Push(line[i]);
+        else
+        {
+            var charOnStack = brackets.Pop();
+            if (charOnStack != GetOpening(line[i]))
+            {
+                errroScore += GetScore(line[i]);
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Result: {errroScore}");
+
+int GetScore(char c)
+{
+    switch (c)
+    {
+        case ')': return 3; 
+        case ']': return 57;
+        case '}': return 1197;
+        case '>': return 25137;
+    }
+
+    return 0;
+}
+
+char GetOpening(char c)
+{
+    switch (c)
+    {
+        case ')': return '(';
+        case ']': return '[';
+        case '}': return '{';
+        case '>': return '<';
+    }
+
+    return ' ';
+}
---
+++ Day10b/Program.cs
@@ -0,0 +1,73 @@
+﻿var input = File.ReadAllLines("input.txt");
+var incompleteLines = new List<string>();
+
+var repairScores = new List<long>();
+foreach (var line in input)
+{
+    var brackets = new Stack<char>();
+    for (int i = 0; i < line.Length; i++)
+    {
+        if(line[i] == '(' || line[i] == '[' || line[i] == '{' || line[i] == '<') brackets.Push(line[i]);
+        else
+        {
+            var charOnStack = brackets.Pop();
+            if (charOnStack != GetOpening(line[i]))
+            {
+                break;
+            }
+        }
+
+        if(i== line.Length - 1)
+        {
+            incompleteLines.Add(line);
+            var score = 0L;
+            for (int n = 0; n < brackets.Count; i++)
+            {
+                score = (5L * score) + GetScore(GetClosing(brackets.Pop()));
+            }
+            repairScores.Add(score);
+        }
+    }
+}
+repairScores.Sort();
+
+Console.WriteLine($"Result: {repairScores.ElementAt((int)repairScores.Count/2)}");
+
+int GetScore(char c)
+{
+    switch (c)
+    {
+        case ')': return 1; 
+        case ']': return 2;
+        case '}': return 3;
+        case '>': return 4;
+    }
+
+    return 0;
+}
+
+char GetOpening(char c)
+{
+    switch (c)
+    {
+        case ')': return '(';
+        case ']': return '[';
+        case '}': return '{';
+        case '>': return '<';
+    }
+
+    return ' ';
+}
+
+char GetClosing(char c)
+{
+    switch (c)
+    {
+        case '(': return ')';
+        case '[': return ']';
+        case '{': return '}';
+        case '<': return '>';
+    }
+
+    return ' ';
+}
